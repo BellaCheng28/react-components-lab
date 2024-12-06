aeeb0d197995abc3738b893b4d2b2d9d47e65bc0
User Name <xiaojiecheng@Xiaojies-MBP.lan>
2024-12-05 22:09:06 -0500

    add weatherForecast component

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,15 @@
+import'./WeatherForecast.css'
+
+
+const WeatherListItem = (props) =>{
+    return (
+<div className="weather">
+  <h2>{props.weatherForecast.day}</h2>
+  <img src={props.weatherForecast.img} alt={props.weatherForecast.imgAlt} />
+  <p><span>conditions: </span>{props.weatherForecast.conditions}</p>
+  <p><span>time: </span>{props.weatherForecast.time}</p>
+</div>
+    )
+}
+
+export default WeatherListItem
